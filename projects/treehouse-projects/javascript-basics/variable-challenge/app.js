7e87a05c15c53fa9bce67cc8f1b5616ae9dba5b7
Noelle Daley <adriannenoelle@gmail.com>
2015-01-08 14:31:25 -0800

    updated madlibs

---
+++ projects/treehouse-projects/javascript-basics/variable-challenge/app.js
@@ -0,0 +1,13 @@
+var nounToChoose = prompt('Please choose a noun.');
+var noun = nounToChoose;
+var adjToChoose = prompt('Please choose an adjective.');
+var adjOne = adjToChoose;
+var adjToChooseTwo = prompt('Please choose a second adjective.');
+var adTwo = adjToChooseTwo;
+var sentenceOne = ('The '+ adjOne + ' ' + noun + ' is ' + adTwo + '.');
+
+var adjToChooseThree = prompt('Please choose a third adjective.');
+var adjThree = adjToChooseThree;
+var nounToChooseTwo = prompt('Please choose a second noun.');
+var nounTwo = nounToChooseTwo;
+var sentenceTwo = ('
