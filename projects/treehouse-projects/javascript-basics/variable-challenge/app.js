fb73e7233a1273e671fa9ee416dee08148020bb3
adriannenoelle@gmail.com <noelledaley@Noelles-MacBook-Pro.local>
2015-01-14 19:14:03 -0800

    added question counter

--- projects/treehouse-projects/javascript-basics/variable-challenge/app.js
+++ projects/treehouse-projects/javascript-basics/variable-challenge/app.js
@@ -1,14 +1,30 @@
-var nounToChoose = prompt('Please choose a noun.');
+var questions = 5;
+var questionsLeft = '[' + questions + ' questions left]';
+var nounToChoose = prompt('Please choose a noun. ' + questionsLeft);
+
+questions -= 1;
+questionsLeft = '[' + questions + ' questions left]';
 var noun = nounToChoose;
-var adjToChoose = prompt('Please choose an adjective.');
+var adjToChoose = prompt('Please choose an adjective. ' + questionsLeft);
+
+questions -= 1;
+questionsLeft = '[' + questions + ' questions left]';
 var adjOne = adjToChoose;
-var adjToChooseTwo = prompt('Please choose a second adjective.');
+var adjToChooseTwo = prompt('Please choose a second adjective. ' + questionsLeft);
 var adTwo = adjToChooseTwo;
 var sentenceOne = ('The '+ adjOne + ' ' + noun + ' is ' + adTwo + '.');
 
-var adjToChooseThree = prompt('Please choose a third adjective.');
+questions -= 1;
+questionsLeft = '[' + questions + ' questions left]';
+var adjToChooseThree = prompt('Please choose a third adjective. ' + questionsLeft);
+
+questions -= 1;
+questionsLeft = '[' + questions + ' questions left]';
 var adjThree = adjToChooseThree;
-var nounToChooseTwo = prompt('Please choose a second noun.');
+var nounToChooseTwo = prompt('Please choose a second noun.' + questionsLeft);
+
+questions -= 1;
+questionsLeft = '[' + questions + ' questions left]';
 var nounTwo = nounToChooseTwo;
 var sentenceTwo = ('In fact, the ' + noun + ' is also ' + adjThree + ' just like a ' + nounTwo + '.');
 document.write(sentenceOne + ' ' + sentenceTwo)
