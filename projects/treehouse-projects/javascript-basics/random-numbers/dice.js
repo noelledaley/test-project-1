0744d808db03c6da2f238f956e63b5dabedbae0a
adriannenoelle@gmail.com <noelledaley@Noelles-MacBook-Pro.local>
2015-01-14 20:30:21 -0800

    updated equation

--- projects/treehouse-projects/javascript-basics/random-numbers/dice.js
+++ projects/treehouse-projects/javascript-basics/random-numbers/dice.js
@@ -1,5 +1,7 @@
+var num1 = prompt('Please type a starting number.');
+var bottomNum = parseInt(num1)
 var num = prompt('Please pick a number.');
 var topNum = parseInt(num);
-var randomNum = Math.floor( Math.random() * topNum) + 1;
-var message = ('<p> ' + randomNum + ' is a number between 1 and ' + topNum + '.</p>');
+var randomNum = Math.floor( Math.random() * (topNum - bottomNum + 1)) + bottomNum;
+var message = ('<p> ' + randomNum + ' is a number between ' + bottomNum + ' and ' + topNum + '.</p>');
 document.write(message);
