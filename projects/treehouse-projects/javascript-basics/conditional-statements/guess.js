586ccbb894b0d9a24eaad266cb1f1c430a180fe2
adriannenoelle@gmail.com <noelledaley@Noelles-MacBook-Pro.local>
2015-01-18 18:38:04 -0800

    typo

--- projects/treehouse-projects/javascript-basics/conditional-statements/guess.js
+++ projects/treehouse-projects/javascript-basics/conditional-statements/guess.js
@@ -1,16 +1,25 @@
 var correctGuess = false;
+
+// generates random number from 1 to 5
 var randomNumber = Math.floor(Math.random() * 5) + 1;
+
 var guess = prompt('I am thinking of a number between 1 and 5. What is it?');
-if ( parseInt(guess) === randomNumber) {
+
+/* test to see if player is
+1. correct
+2. guessed too high
+3. guessed too low
+*/
+if (parseInt(guess) === randomNumber) {
     correctGuess = true;
-} else if (parseInt(guess) < randomNumber ) {
+} else if ( parseInt(guess) < randomNumber ) {
     var guessMore = prompt('Try again. The number I am thinking of is more than ' + guess);
-    if ( parseInt(guessMore) === randomNumber) {
+    if (parseInt(guessMore) === randomNumber) {
         correctGuess = true;
     }
 } else if ( parseInt(guess) > randomNumber) {
  var guessLess = prompt('Try again. The number I am thinking of is less than ' + guess);
-    if parseInt(guessLess) === randomNumber) {
+    if (parseInt(guessLess) === randomNumber) {
         correctGuess = true;
     }
 }
