ddd8687ffca47f636988a2e6df2b6f090faee42a
adriannenoelle@gmail.com <noelledaley@Noelles-MacBook-Pro.local>
2015-01-18 18:28:36 -0800

    added hints

--- projects/treehouse-projects/javascript-basics/conditional-statements/guess.js
+++ projects/treehouse-projects/javascript-basics/conditional-statements/guess.js
@@ -1,6 +1,20 @@
+var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 5) + 1;
 var guess = prompt('I am thinking of a number between 1 and 5. What is it?');
-if (parseInt(guess) === randomNumber) {
+if ( parseInt(guess) === randomNumber) {
+    correctGuess = true;
+} else if (parseInt(guess) < randomNumber ) {
+    var guessMore = prompt('Try again. The number I am thinking of is more than ' + guess);
+    if ( parseInt(guessMore) === randomNumber) {
+        correctGuess = true;
+    }
+} else if ( parseInt(guess) > randomNumber) {
+ var guessLess = prompt('Try again. The number I am thinking of is less than ' + guess);
+    if parseInt(guessLess) === randomNumber) {
+        correctGuess = true;
+    }
+}
+if ( correctGuess ) {
     document.write('<p>You guessed the number!</p>');
 } else {
     document.write('<p>Sorry. The number was ' + randomNumber + '.</p>');
