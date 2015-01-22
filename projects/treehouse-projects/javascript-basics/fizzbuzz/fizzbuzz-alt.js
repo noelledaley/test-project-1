17369b187e5a0615d490f0c0303ad347ee065470
Noelle Daley <adriannenoelle@gmail.com>
2015-01-22 14:30:34 -0800

    updated function

--- projects/treehouse-projects/javascript-basics/fizzbuzz/fizzbuzz-alt.js
+++ projects/treehouse-projects/javascript-basics/fizzbuzz/fizzbuzz-alt.js
@@ -1,8 +1,12 @@
+var skipMultiples = function() {
     for (var i = 1; i <= 50; i++) {
-    if (i % 7 == 0) {
-        console.log('There is power in looking silly and not caring that you do.');
-    } else {
-        console.log(i);
+        if (i % 7 == 0) {
+            console.log('There is power in looking silly and not caring that you do.');
+        } else {
+            console.log(i);
+        }
     }
-}
-    
+} 
+
+skipMultiples();
+
