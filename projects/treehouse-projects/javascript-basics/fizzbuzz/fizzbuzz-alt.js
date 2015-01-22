0209df1aeaf0fde01138d31de1d876ea4947eb9f
Noelle Daley <adriannenoelle@gmail.com>
2015-01-21 20:29:55 -0800

    added fizzbuzz alternate version

---
+++ projects/treehouse-projects/javascript-basics/fizzbuzz/fizzbuzz-alt.js
@@ -0,0 +1,7 @@
+for (var i = 1; i <= 50; i++) {
+    if (i % 7 == 0) {
+        console.log('There is power in looking silly and not caring that you do.');
+    } else {
+        console.log(i);
+    }
+}
