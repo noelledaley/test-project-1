b944f79ae506040f768c6b4f618f6431029b9749
Noelle Daley <adriannenoelle@gmail.com>
2015-01-22 16:07:21 -0800

    fixed typos

--- projects/treehouse-projects/javascript-basics/conditional-challenge/quiz.js
+++ projects/treehouse-projects/javascript-basics/conditional-challenge/quiz.js
@@ -2,27 +2,27 @@ var correctGuess = 0;
 
 //ask questions
 var q1 = prompt('What is my favorite color?');
-    if (q1.toUpperCase()== 'GREEN') {
-        var correctGuess +=1 ;
+    if ( q1.toUpperCase() == 'GREEN') {
+        var correctGuess +=1;
     }
 
 var q2 = prompt('What is my favorite food?');
-    if (q2.toUpperCase() == 'BANANA') {
+    if ( q2.toUpperCase() == 'BANANA') {
         var correctGuess +=1;
     }
 
 var q3 = prompt('What is my favorite animal?');
-    if (q3.toUpperCase() == 'WHALE') {
+    if ( q3.toUpperCase() == 'WHALE') {
         var correctGuess +=1;
     }
 
 var q4 = prompt('What instrument do I play?');
-    if (q4.toUpperCase() == 'GUITAR') {
+    if ( q4.toUpperCase() == 'GUITAR') {
         var correctGuess +=1;
     }
 
 var q5 = prompt('Where was I born?');
-    if (q5.toUpperCase() == 'SAN FRANCISCO') {
+    if ( q5.toUpperCase() == 'SAN FRANCISCO') {
         var correctGuess +=1;
     }
 
