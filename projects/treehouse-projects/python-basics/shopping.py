68a26672a5c48987807bea48b102868759455c8d
Noelle Daley <adriannenoelle@gmail.com>
2015-02-05 20:02:32 -0800

    typo

--- projects/treehouse-projects/python-basics/shopping.py
+++ projects/treehouse-projects/python-basics/shopping.py
@@ -6,14 +6,14 @@ print('Enter "DONE" to stop adding items.')
 while True:
     new_item = input("> ")
     
-    if new_item == 'DONE';
+    if new_item == 'DONE':
       break
     
     shopping_list.append(new_item)
     print("Added! List has {} items.".format(len(shopping_list)))
     continue
 
-print('Here is your list:")
+print('Here is your list:')
 
 for item in shopping_list:
       print(item)
