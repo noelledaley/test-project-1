93a8661885ee2a1e7e77724c0ec324a9433bde53
Noelle Daley <adriannenoelle@gmail.com>
2015-02-07 14:25:42 -0800

    added arrays & for loops

--- projects/nodeschool/javascripting/if-statement.js
+++ projects/nodeschool/javascripting/if-statement.js
@@ -1,2 +1,7 @@
 var fruit = 'orange'
-console.log('The fruit name has more than five characters.')
+
+if (fruit.length > 5) {
+	console.log('The fruit name has more than five characters.')
+} else {
+	console.log('The fruit name has five characters or less.')
+}
