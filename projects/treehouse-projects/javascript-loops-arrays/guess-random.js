a66fe71c6700dd077ab10f26b2e0a08e89e56c94
Noelle Daley <adriannenoelle@gmail.com>
2015-02-13 12:04:59 -0800

    added loops

--- projects/treehouse-projects/javascript-loops-arrays/guess-random.js
+++ projects/treehouse-projects/javascript-loops-arrays/guess-random.js
@@ -1,4 +1,15 @@
+var upper = 10000;
+var randomNumber = getRandomNumber(upper);
+var guess;
+var attempts = 0;
+
 // generate random number between 1 and 10,000
 function getRandomNumber(upper) {
     return Math.floor(Math.random() * upper) + 1;
+}
+
+// compare computer's guess to randomNumber
+while (guess !== randomNumber) { //while computer's guess is not true
+    guess = getRandomNumber(upper);
+    attempts += 1;
 }
