ff11747443546aea332af043df92a7314b7bcef6
adriannenoelle@gmail.com <noelledaley@Noelles-MacBook-Pro.local>
2015-03-08 19:11:29 -0700

    added comments

--- projects/treehouse-projects/javascript-loops-arrays/do-while.js
+++ projects/treehouse-projects/javascript-loops-arrays/do-while.js
@@ -1,17 +1,20 @@
 var randomNumber = getRandomNumber(10);
 var guess;
 var guessCount = 0;
-var correctGuess = false;
+var correctGuess = false; // starts out false so the loop will run
 
 function getRandomNumber(upper) {
     var num = Math.floor(Math.random() * upper) + 1;
     return num;
 }
 
-do {
+do { // do this
     guess = prompt('I am thinking of a number between 1 and 10. What is it?');
     guessCount +=1;
     if (parseInt(guess) === randomNumber) {
         correctGuess = true;
     }
-} while ( ! correctGuess )
+} while ( ! correctGuess ) // while the correct guess is false
+    
+document.write('<h1>You guessed the number!<h1>');
+document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber + ' .')
